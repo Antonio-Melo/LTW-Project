8d1d7650bba2f5fda60521076b10c0fbb69f2bdb
lazarocosta <lazaroosta@hotmail.com>
2016-11-26 00:36:11 +0000

    created conection to database

---
+++ database/connection.php
@@ -0,0 +1,6 @@
+<?php
+
+$db = new PDO('sqlite:database/restaurante.db');
+
+//http://gnomo.fe.up.pt/~up201405342/projeto/database/connection.php -->
+?>
---
+++ database/restaurante.sql
@@ -0,0 +1,3 @@
+-- sqlite3 -init restaurante.sql restaurante.db  ->comando para criar a base de dados
+
+--criar as tabelas e inserir os elementos aqui
