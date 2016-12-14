574f4c162e6a1e3b97d0c5dca6d08bf6a287c4c8
Antonio-Melo <happyd3nt@gmail.com>
2016-12-14 11:31:36 +0000

    working?

--- templates/searchResults.php
+++ templates/searchResults.php
@@ -20,5 +20,6 @@
                 <a href="../pages/listRestaurant.php?id=<?php echo $id ?>"> <?php echo $name ?> </a>
             </li>
         <?php }}else echo "No results found";?>
+
     </ul>
 </div>
