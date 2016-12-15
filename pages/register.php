69a803d21c1be06fd33ab1e1e0a417203d79a1af
margaridaviterbo <margaridaviterbo@hotmail.com>
2016-12-15 05:47:12 +0000

    merdas

--- pages/register.php
+++ pages/register.php
@@ -1,4 +1,5 @@
 <?php
+$cssPath = '../style/register.css';
   include ('../templates/header.php');
   include ('../templates/register.php');
   include ('../templates/footer.php');
--- templates/register.php
+++ templates/register.php
@@ -2,16 +2,17 @@
         <input type="text" name="username" required="required"  placeholder= "Username"><br>
 
         <input type="text" name="name"  placeholder= "Name"><br>
+        <div id="mail">
+          <input type="e-mail" name="email" required="required" placeholder= "Email address" pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})" title="Invalid Email!" placeholder= "Email"/><br>
 
-        <input type="e-mail" name="email" required="required" placeholder= "Email address" pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})" title="Invalid Email!" placeholder= "Email"/><br>
+          <input type="password"  name="password" required="required" pattern=".{5,}" title="Password must be at least 5 characters long!" placeholder= "Password"/><br>
 
-        <input type="password"  name="password" required="required" pattern=".{5,}" title="Password must be at least 5 characters long!" placeholder= "Password"/><br>
+          <input type="password" name="confirm" required="required" placeholder= "Confirm Password"/><br>
 
-        <input type="password" name="confirm" required="required" placeholder= "Confirm Password"/><br>
+          <input type="date" name="dateBirth" required="required" placeholder= "Date of Birth"/><br>
 
-        <input type="date" name="dateBirth" required="required" placeholder= "Date of Birth"/><br>
+            Avatar <input type="file" name="avatar" id="fileToUpload"><br>
 
-        Avatar <input type="file" name="avatar" id="fileToUpload"><br>
-
-      <input class="submit" type="submit" value="Register">
+          <input class="submit" type="submit" value="Register">
+        </div>
     </form>
